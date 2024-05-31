b499dc8d72fd8559c402e02bb6cc0b87abd08b7a
RyanYang99 <diddbstjd0220@naver.com>
2024-05-31 19:08:04 +0900

    앱 작스에 메인페이지 작스 연결

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,1 +1,16 @@
-/* 메인페이지 생성 */
+import React from 'react';
+
+const MainHeader = styled.div`
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+`;
+
+function MainPage() {
+    return <MainHeader></MainHeader>;
+}
+
+export default MainPage;
