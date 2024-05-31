80fd8bb8a2018a873d0426ae311d65eb58678810
LastDice <mkbk8912@naver.com>
2024-05-31 16:10:05 +0900

    daisyUI 로 UI 간단하게 꾸밈.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ const supabase = createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rem91bm52ZGVqdndlYW15enNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxMzU2MDYsImV4cCI6MjAzMjcxMTYwNn0.0gOgO3J5ybGbHANtLp9xe-QpmS-CL1EVxG1mcyBqHzw"
 );
 
+const Providers = {
+    Discord: 'discord',
+    Github: 'github',
+    Google: 'google',
+    Kakao: 'kakao',
+    Notion: 'notion',
+    Twitch: 'twitch',
+    Zoom: 'zoom',
+}
+Object.freeze(Providers);
+
 function App() {
     const [posts, setPosts] = useState([]);
     const [signIn, setSignIn] = useState(false);
@@ -15,12 +26,6 @@ function App() {
         setPosts(data);
     }
 
-    async function signInWithGithub() {
-        await supabase.auth.signInWithOAuth({
-            provider: 'github',
-        });
-    }
-
     async function checkSignIn() {
         const session = await supabase.auth.getSession();
         const isSignIn = !!session.data.session;
@@ -28,31 +33,36 @@ function App() {
         setSignIn(isSignIn);
     }
 
-    async function signOut() {
-        await supabase.auth.signOut();
-        checkSignIn();
-    }
-
     useEffect(() => {
-        getPosts();
         checkSignIn();
+        getPosts();
     }, []);
 
     return (
         <div className="flex flex-col space-y-5 p-14">
             {signIn ? (
-                <button type="button" onClick={signOut}>로그아웃</button>
+                <button type="button" onClick={async () => {
+                    await supabase.auth.signOut();
+                    await checkSignIn();
+                }} className="btn btn-primary">로그아웃</button>
             ) : (
-                <button type='button' onClick={signInWithGithub}>로그인</button>
+                <button type='button' onClick={async ()=> {
+                    await supabase.auth.signInWithOAuth({
+                        provider: Providers.Github,
+                    });
+                    await checkSignIn();
+                }} className="btn btn-info">로그인</button>
             )}
-            <ul>
+            <div className="grid grid-cols-4 gap-4 bg-white shadow-2xl p-5 rounded-2xl">
                 {posts.map((post) => (
-                    <div key={post.id} className="flex flex-col p-4 bg-blue-300 rounded-2xl text-black space-y-2">
-                        <h2 className="font-bold">{post.title}</h2>
-                        <p>{post.content}</p>
+                    <div className="card bg-base-100 shadow-xl" key={post.id}>
+                        <div className="card-body">
+                            <h2 className="card-title">{post.title}</h2>
+                            <p>{post.content}</p>
+                        </div>
                     </div>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 }
